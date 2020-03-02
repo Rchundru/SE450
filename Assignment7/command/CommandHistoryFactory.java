cae289b2d98a8f154330ebdd80d283c8efab1f44
Rchundru <rohitchundru@gmail.com>
2020-03-02 12:04:38 -0600

    Add files via upload

---
+++ Assignment7/command/CommandHistoryFactory.java
@@ -0,0 +1,10 @@
+package shop.command;
+
+public class CommandHistoryFactory {
+  private CommandHistoryFactory() {}
+  static public CommandHistory newCommandHistory() {
+    // TODO  
+	  CommandHistory cmd = new CommandHistoryObj();
+	  return cmd;
+  }
+}
